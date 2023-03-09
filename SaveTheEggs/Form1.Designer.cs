4e34cc75be5f69faa93ed3665e2d6afe21fefc36
BStainz <brendanhannon1011@gmail.com>
2023-03-08 20:33:48 -0500

    Add project files.

---
+++ SaveTheEggs/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SaveTheEggs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtMissed = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SaveTheEggs.Properties.Resources.egg;
+            this.pictureBox1.Location = new System.Drawing.Point(275, 107);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "eggs";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SaveTheEggs.Properties.Resources.egg;
+            this.pictureBox2.Location = new System.Drawing.Point(478, 107);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "eggs";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::SaveTheEggs.Properties.Resources.egg;
+            this.pictureBox3.Location = new System.Drawing.Point(84, 107);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "eggs";
+            // 
+            // Player
+            // 
+            this.Player.Image = global::SaveTheEggs.Properties.Resources.chicken_normal;
+            this.Player.Location = new System.Drawing.Point(264, 578);
+            this.Player.Margin = new System.Windows.Forms.Padding(5);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(85, 80);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 3;
+            this.Player.TabStop = false;
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Location = new System.Drawing.Point(14, 16);
+            this.txtScore.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(110, 29);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // txtMissed
+            // 
+            this.txtMissed.AutoSize = true;
+            this.txtMissed.Location = new System.Drawing.Point(473, 16);
+            this.txtMissed.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.txtMissed.Name = "txtMissed";
+            this.txtMissed.Size = new System.Drawing.Size(125, 29);
+            this.txtMissed.TabIndex = 4;
+            this.txtMissed.Text = "Missed: 0";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(612, 693);
+            this.Controls.Add(this.txtMissed);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Save The Eggs";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtMissed;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ SaveTheEggs/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaveTheEggs
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight;
+        int speed = 8;
+        int score = 0;
+        int missed = 0;
+
+        Random randX = new Random();
+        Random randY = new Random();
+
+        PictureBox splash = new PictureBox();
+
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Score: " + score;
+            txtMissed.Text = "Missed: " + missed;
+
+            if (goLeft == true && Player.Left > 0) // if left key is pushed and player isnt at the end of board  
+            {
+                Player.Left -= 12; //speed set to 12
+                Player.Image = Properties.Resources.chicken_normal2; //image of player switches to chicken facing left
+            }
+            if (goRight == true && Player.Left + Player.Width < this.ClientSize.Width)// if right key is pushed and player isnt at the end of board 
+            {
+                Player.Left += 12; //speed set to 12
+                Player.Image = Properties.Resources.chicken_normal; //image of player switches to chicken facing right
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag =="eggs") //pushes eggs down
+                {
+                    x.Top += speed;
+                    
+                    if(x.Top + x.Height >this.ClientSize.Height) // if egg left the scene it resets them back to the top
+                    {
+                        splash.Image = Properties.Resources.splash; //splash image of cracked egg if it leaves client
+                        splash.Location = x.Location; //splash image will be at same location as the egg
+                        splash.Height = 60;
+                        splash.Width = 60;
+                        splash.BackColor = Color.Transparent;
+
+                        this.Controls.Add(splash);
+
+                        x.Top = randY.Next(80, 300) * -1; 
+                        x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                        missed += 1; //missed score increments
+                        Player.Image = Properties.Resources.chicken_hurt;  // player image turns to hurt chicken.
+                    }
+                    if (Player.Bounds.IntersectsWith(x.Bounds)) // if player and egg image intersect
+                    {
+                        x.Top = randY.Next(80, 300) * -1; //egg resets at the top
+                        x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                        score += 1; // score increased
+                    }
+                 }
+            }
+            if (score >10)
+            {
+                speed = 12;
+            }
+            if (missed > 5)
+            {
+                GameTimer.Stop();
+                MessageBox.Show("Game Over" + Environment.NewLine + "We've Lost good Eggs!" + Environment.NewLine + "Click ok to retry");
+                RestartGame();
+            }
+            //Brendan Hannon CPS-3330-01 Spring2023 Final Project
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) // if left key clicked go left
+            { 
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right) // if right key clicked go right
+            {
+                goRight = true;
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) //if left key released set to false
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right) // if right key released set to false
+            {
+                goRight =false; 
+            }
+        }
+
+        private void RestartGame()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "eggs") //check for the eggs tag in top 3 picture box
+                {
+                    x.Top = randY.Next(80, 300) * -1; // Gives random X coordinate. Set to negative so Timer brings down towards player
+                    x.Left = randX.Next(5, this.ClientSize.Width - x.Width); //Random Y Coordinate 5 from left or right
+                                                                             //Brendan Hannon CPS-3330-01 Spring2023 Final Project
+                }
+            }
+
+            Player.Left = this.ClientSize.Width / 2; //centers the player image of the chicken
+            Player.Image = Properties.Resources.chicken_normal; //loads chicken face to the right as default player
+
+            score = 0; // resets score to 0
+            missed = 0; // resets missed to 0
+            speed = 8; //sets speed to 8
+
+            goLeft = false; //resets direction player is going to false
+            goRight = false; //resets direction player is going to false
+
+            GameTimer.Start(); //starts game timer
+        }
+    }
+}
